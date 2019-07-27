d0bf60ef969babfd575ac72d9e8a500b9482ebef
Matt Gilbertson <gilbe415@umn.edu>
2019-07-27 10:51:33 -0500

    updated Contra keymap
    deleted old bits in Contra's keymap.c

--- keyboards/contra/keymaps/Choco617/keymap.c
+++ keyboards/contra/keymaps/Choco617/keymap.c
@@ -20,41 +20,6 @@
 #include "action_layer.h"
 #include "Choco617.h"
 
-// #define EID "oldID"
-// #define PW "oldpass"
-// #define EID2 KC_SECRET_1
-// #define PW2 KC_SECRET_2
-// #define CAD LCTL(LALT(KC_DEL))
-
-// #define KC_TDQ TD(TD_Q)
-// #define KC_TDS TD(TD_SLS)
-// #define KC_SHENT SFT_T(KC_ENT)
-// #define KC_SHCAPS SFT_T(KC_CAPS)
-// #define KC_SHBS SFT_T(KC_BSPC)
-// #define KC_POWR MO(_POWER)
-// #define KC_LOWR LOWER
-// #define KC_RASE RAISE
-
-// #define KC_SPARR LT(_POWER, KC_SPC)
-// #define KC_RAENT LT(_RAISE, KC_ENT)
-// #define KC_LODEL LT(_LOWER, KC_DEL)
-// #define KC_MADJ MO(_ADJUST)
-// #define KC_ACTL CTL_T(KC_A)
-// #define KC_RRWIN GUI_T(KC_R) // KC_RWIN is already defined
-// #define KC_SALT ALT_T(KC_S)
-// #define KC_TSHF SFT_T(KC_T)
-// #define KC_NSHF SFT_T(KC_N)
-// #define KC_EALT ALT_T(KC_E)
-// #define KC_IWIN GUI_T(KC_I)
-// #define KC_OCTL CTL_T(KC_O)
-// #define KC_PCTL CTL_T(KC_P)
-
-// #define KC_TDL TD(TD_LPN)
-// #define KC_TDR TD(TD_RPN)
-// #define KC_TDC TD(TD_COLON)
-
-// #define KC_DELE DelEnd
-
 #define KC_LGN1 TD(TD_LGN)
 #define KC_LGN2 LoginEIDPW
 #define KC_CAD LCTL(LALT(KC_DEL))
@@ -70,26 +35,11 @@
 #define KC_FF6 KC_F6
 #define KC_FF7 KC_F7
 
-// #define KC_EX_GAM EXT_GAM
-
 extern keymap_config_t keymap_config;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-// Colemak-DHm
-
-/*
-[_COLEMAK] = LAYOUT_kc(
-//┌───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┐
-   TAB,    TDQ,    W,      F,      P,      B,      J,      L,      U,      Y,      SCLN,   BSPC,
-   ESC,    A,      R,      S,      T,      G,      M,      N,      E,      I,      O,      QUOT,
-   SHCAPS, Z,      X,      C,      D,      V,      K,      H,      COMM,   DOT,    TDS,    SHENT,
-   LCTL,   LGUI,   LALT,   POWR,   LOWR,   SHBS,   SPC,    RASE,   LEFT,   DOWN,   UP,     RGHT
-//└───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┘
-),
-*/
-
-
+// Colemak-DHm, regular, with corner modifiers and arrows
 [_COLEMAK] = LAYOUT_wrapper(
 //┌───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┐
    TAB,    ______________COLEMAK_L1_____________,  ______________COLEMAK_R1_____________,  BSPC,
@@ -100,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 
-// Colemak adventurous, Windows
+// Colemak-DHm with Windows home row mods
 [_COLEMAKWIN] = LAYOUT_wrapper(
 //┌───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┐
    TAB,    __________COLEMAK_ADV_WIN_L1_________,  __________COLEMAK_ADV_WIN_R1_________,  BSPC,
@@ -110,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //└───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┘
 ),
 
-// Colemak adventurous, macOS
+// Colemak-DHm with macOS home row mods
 [_COLEMAKMAC] = LAYOUT_wrapper(
 //┌───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┐
    TAB,    __________COLEMAK_ADV_MAC_L1_________,  __________COLEMAK_ADV_MAC_R1_________,  BSPC,
@@ -129,18 +79,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //└───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┘
 ),
 
-// Lower
-/*
-[_LOWER] = LAYOUT_kc(
-//┌───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┐
-   GRV,    NO,     NO,     BSLS,   AMPR,   PSCR,   NO,     PSLS,   P7,     P8,     P9,     PAST,
-   TRNS,   MINUS,  EQL,    TDL,    TDR,    SLCK,   HOME,   PGUP,   P4,     P5,     P6,     PMNS,
-   TRNS,   MPRV,   MPLY,   MNXT,   VOLD,   PAUS,   END,    PGDN,   P1,     P2,     P3,     PPLS,
-   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   DEL,    TRNS,   MADJ,   P0,     P0,     TDC,    PENT
-//└───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┘
-),
-*/
-
 [_LOWER] = LAYOUT_wrapper(
 //┌───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┐
    GRV,    ______________LOWER_L1_______________,  ______________LOWER_R1_______________,  PGUP,
@@ -150,20 +88,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //└───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┘
 ),
 
-
-// Raise
-/*
-[_RAISE] = LAYOUT_kc(
-//┌───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┐
-   GRV,    1,      2,      3,      4,      5,      6,      7,      8,      9,      0,      TRNS,
-   TRNS,   F1,     F2,     F3,     F4,     F5,     F6,     LBRC,   INS,    HOME,   PGUP,   TRNS,
-   TRNS,   F7,     F8,     F9,     F10,    F11,    F12,    RBRC,   DEL,    END,    PGDN,   TRNS,
-   TRNS,   TRNS,   TRNS,   TRNS,   MADJ,   DELE,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS
-//└───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┘
-),
-*/
-// note - the layer should be this:
-
 [_RAISE] = LAYOUT_wrapper(
 //┌───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┐
    GRV,    ______________RAISE_L1_______________,  ______________RAISE_R1_______________,  TRNS,
@@ -172,17 +96,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    NO,     NO,     NO,     NO,     MADJ,   DELE,   TRNS,   TRNS,   NO,     NO,     NO,     NO 
 //└───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┘
 ),
-
-/*
-[_RAISE] = LAYOUT_wrapper(
-//┌───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┐
-   GRV,    1,      2,      3,      4,      5,      6,      7,      8,      9,      0,      TRNS,
-   TRNS,   1,      2,      3,      4,      5,      6,      LBRC,   INS,    HOME,   PGUP,   TRNS,
-   TRNS,   FF7,    F8,     F9,     F10,    F11,    F12,    RBRC,   DEL,    END,    PGDN,   TRNS,
-   NO,     NO,     NO,     NO,     MADJ,   DELE,   TRNS,   TRNS,   NO,     NO,     NO,     NO 
-//└───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┘
-),
-*/
 
 /* Adjust (Lower + Raise)
  *  Alt "macros" above raise/lower are for JDE:
@@ -218,17 +131,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //└───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┘
 ),
 
-/* Gaming layer
- * ,-----------------------------------------------------------------------------------.
- * |  1   | Tab  |  Q   |  W   |  E   |  R   |  T   |      |      | Ins  | Home | PgUp |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |  2   | Esc  |  A   |  S   |  D   |  F   |  G   |      |      | Del  | End  | PgDn |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |  3   | Shft |  Z   |  X   |  C   |  V   |      |      |      | Exit |  Up  |Enter |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |  4   | Ctrl |      |      | Spce | Spce | Spce | Spce |      | Left |  Dn  |Right |
- * `-----------------------------------------------------------------------------------'
- */
 [_GAMING] = LAYOUT_kc(
 //┌───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┐
    1,      TAB,    Q,      W,      E,      R,      T,      NO,     NO,     INS,    HOME,   PGUP,
@@ -259,5 +161,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
 };
-
-
